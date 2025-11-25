b40f348f9441e7ee532fdeb90a4a0066e6d09837
pablohigu <pablo.hi.do@gmail.com>
2025-11-25 18:49:14 +0100

    CAMBIOS

--- src/app/features/auth/status-message/status-message.ts
+++ src/app/features/auth/status-message/status-message.ts
@@ -7,14 +7,14 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './status-message.html',
-  styleUrls: []
+  styleUrls: [] 
 })
 export class StatusMessageComponent {
-  // Usamos nombres específicos 'card...' para que no se mezclen con el layout
+  // CAMBIO IMPORTANTE: Usamos 'cardTitle' y 'cardMessage' para evitar conflictos
   @Input() cardTitle: string = '¡Solicitud enviada!';
   
   @Input() cardMessage: string = 'Pronto recibirás un correo electrónico a la dirección que nos has proporcionado con la resolución de tu petición.';
   
   @Input() showContactLink: boolean = true;
-  @Input() backLink: string = ''; 
+  @Input() backLink: string = '/login';
 }
